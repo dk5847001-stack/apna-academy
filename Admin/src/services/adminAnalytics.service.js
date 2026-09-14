9b32a231a2d91882484c47d672f3a8c52893f83e
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 14:01:54 +0530

    feat(admin): support analytics filters

--- Admin/src/services/adminAnalytics.service.js
+++ Admin/src/services/adminAnalytics.service.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-export const getAdminAnalytics = async () => {
-  const response = await api.get("/admin/analytics");
+export const getAdminAnalytics = async (params = {}) => {
+  const response = await api.get("/admin/analytics", { params });
   return response?.data?.data ?? response?.data ?? {};
 };
