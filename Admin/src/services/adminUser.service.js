7b7814f400b4586600afc8775eb6de5f8ba4a948
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 14:08:35 +0530

    feat(admin): add user details service

--- Admin/src/services/adminUser.service.js
+++ Admin/src/services/adminUser.service.js
@@ -1,7 +1,6 @@
 import api from "./api";
-
 const unwrap = (response) => response?.data?.data ?? response?.data ?? {};
-
 export const listAdminUsers = async (params = {}) => unwrap(await api.get("/admin/users", { params }));
 export const getAdminUser = async (userId) => unwrap(await api.get(`/admin/users/${userId}`));
+export const getAdminUserDetails = async (userId) => unwrap(await api.get(`/admin/users/${encodeURIComponent(userId)}/details`));
 export const updateAdminUser = async (userId, payload) => unwrap(await api.patch(`/admin/users/${userId}`, payload));
