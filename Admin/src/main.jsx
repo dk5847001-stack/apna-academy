78f28fb115fde7f04063ebdf896d7ff4ab8e7601
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 11:23:07 +0530

    feat: expose admin users management view

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Users from './pages/Users.jsx'
+
+const Root = () => {
+  const showUsers = window.location.hash === '#users'
+  return showUsers ? <Users /> : <App />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
