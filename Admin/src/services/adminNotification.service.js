c608ae86af5f4777695008859ae952962fc8cddd
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 12:10:59 +0530

    feat: add admin notification api service

---
+++ Admin/src/services/adminNotification.service.js
@@ -0,0 +1,9 @@
+import api from "./api";
+
+const unwrap = (response) => response?.data?.data ?? response?.data ?? {};
+
+export const listAdminNotifications = async (params = {}) => unwrap(await api.get("/admin/notifications", { params }));
+
+export const createAdminNotification = async (payload) => unwrap(await api.post("/admin/notifications", payload));
+
+export const deleteAdminNotification = async (notificationId) => unwrap(await api.delete(`/admin/notifications/${notificationId}`));
