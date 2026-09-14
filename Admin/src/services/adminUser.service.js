1373de03669f01a51e6f3e6f4532f9466a50d9cc
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 11:21:46 +0530

    feat: add admin user api client

---
+++ Admin/src/services/adminUser.service.js
@@ -0,0 +1,7 @@
+import api from "./api";
+
+const unwrap = (response) => response?.data?.data ?? response?.data ?? {};
+
+export const listAdminUsers = async (params = {}) => unwrap(await api.get("/admin/users", { params }));
+export const getAdminUser = async (userId) => unwrap(await api.get(`/admin/users/${userId}`));
+export const updateAdminUser = async (userId, payload) => unwrap(await api.patch(`/admin/users/${userId}`, payload));
