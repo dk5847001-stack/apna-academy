41e276fd4327a4e1c56ea69403c1851189d0c2eb
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 19:41:26 +0530

    Wire navbar search and dark-mode functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 
 import AuthNavbarSync from "./components/AuthNavbarSync";
+import NavbarFunctionality from "./components/NavbarFunctionality";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <NavbarFunctionality />
       <AuthNavbarSync />
       <AppRoutes />
     </BrowserRouter>
