1f479a328fe2ba39a701e09dd9ebffb556700a8c
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 10:51:51 +0530

    feat: configure admin tailwind vite app

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -1,7 +1,21 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "localhost",
+    port: 5176,
+    strictPort: true,
+  },
+  preview: {
+    host: "localhost",
+    port: 5176,
+    strictPort: true,
+  },
+  build: {
+    target: "es2022",
+    sourcemap: false,
+  },
+});
