3fba73b240fc8bf54a69ee895a23980fb538a5ec
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-14 19:26:48 +0530

    fix frontend authenticated navbar navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 
+import AuthNavbarSync from "./components/AuthNavbarSync";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <AuthNavbarSync />
       <AppRoutes />
     </BrowserRouter>
   );
