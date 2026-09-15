df7c4080f20886262a408f7816a54983fc6e5491
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-15 10:30:01 +0530

    feat(auth): add forgot and reset password routes

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,13 +1,36 @@
-import {Route,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import About from "../pages/About";
 import Blog from "../pages/Blog";
 import ContactNew from "../pages/ContactNew";
 import Courses from "../pages/Courses";
+import ForgotPassword from "../pages/ForgotPassword";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
 import PrivacyPolicy from "../pages/PrivacyPolicy";
 import RefundPolicy from "../pages/RefundPolicy";
 import Register from "../pages/Register";
-export default function AppRoutes(){return <Routes><Route element={<MainLayout/>}><Route path="/" element={<Home/>}/><Route path="/courses" element={<Courses/>}/><Route path="/about" element={<About/>}/><Route path="/contact" element={<ContactNew/>}/><Route path="/blog" element={<Blog/>}/><Route path="/privacy-policy" element={<PrivacyPolicy/>}/><Route path="/refund-policy" element={<RefundPolicy/>}/><Route path="/login" element={<Login/>}/><Route path="/register" element={<Register/>}/></Route><Route path="*" element={<NotFound/>}/></Routes>}
+import ResetPassword from "../pages/ResetPassword";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactNew />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/refund-policy" element={<RefundPolicy />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
