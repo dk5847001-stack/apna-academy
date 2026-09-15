ec064d2ceb8e219fc1912e80cdae383655b08a0b
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-15 10:29:16 +0530

    feat(auth): add forgot password page

---
+++ Frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import {
+  Alert,
+  ArrowBackIcon,
+  Button,
+  CircularProgress,
+  EmailIcon,
+  LockResetIcon,
+  SchoolIcon,
+  TextField,
+} from "@mui/material";
