f9491ac691de9ac3430195227bd32df9ed11700d
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-16 14:21:54 +0530

    fix(course-player): scope page overflow lock to learning route

--- Course/src/main.jsx
+++ Course/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,9 +6,26 @@ import "./index.css";
 import App from "./App";
 import CertificatePreviewMount from "./components/CertificatePreviewMount";
 
+function CourseLearningViewportLock() {
+  useEffect(() => {
+    const isLearningRoute = /^\/courses\/[^/]+\/learn(?:\/|$)/.test(window.location.pathname);
+
+    if (!isLearningRoute) return undefined;
+
+    document.body.classList.add("apna-course-learning-page");
+
+    return () => {
+      document.body.classList.remove("apna-course-learning-page");
+    };
+  }, []);
+
+  return null;
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <CourseLearningViewportLock />
       <App />
       <CertificatePreviewMount />
     </BrowserRouter>
