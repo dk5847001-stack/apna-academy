aa142a3fa70c10c2146e021e551c9e211613d200
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-17 21:36:22 +0530

    SEO: organize Course production assets for crawl-safe deployment

--- Course/vite.config.js
+++ Course/vite.config.js
@@ -7,4 +7,22 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name || "asset";
+          if (/\.(?:png|jpe?g|gif|svg|webp|avif)$/i.test(name)) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+          if (/\.css$/i.test(name)) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+  },
 });
