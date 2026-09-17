8865f10215ab500c1349eb6d875188cb8922a107
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-17 14:44:17 +0530

    SEO: wire private dashboard SEO guard

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
+import SeoGuard from "./components/SeoGuard";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <SeoGuard />
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
