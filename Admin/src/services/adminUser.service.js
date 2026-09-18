d79c96cff958f645bd909ab8b7bbb65d57b91541
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-18 16:15:49 +0530

    feat: add admin user account action service methods

--- Admin/src/services/adminUser.service.js
+++ Admin/src/services/adminUser.service.js
@@ -4,3 +4,9 @@ export const listAdminUsers = async (params = {}) => unwrap(await api.get("/admi
 export const getAdminUser = async (userId) => unwrap(await api.get(`/admin/users/${userId}`));
 export const getAdminUserDetails = async (userId) => unwrap(await api.get(`/admin/users/${encodeURIComponent(userId)}/details`));
 export const updateAdminUser = async (userId, payload) => unwrap(await api.patch(`/admin/users/${userId}`, payload));
+
+export const blockAdminUser = async (userId, reason = "") => unwrap(await api.post(`/admin/users/${userId}/block`, { reason }));
+export const unblockAdminUser = async (userId) => unwrap(await api.post(`/admin/users/${userId}/unblock`));
+export const suspendAdminUser = async (userId, reason = "") => unwrap(await api.post(`/admin/users/${userId}/suspend`, { reason }));
+export const activateAdminUser = async (userId) => unwrap(await api.post(`/admin/users/${userId}/activate`));
+export const unfreezeAdminUser = async (userId) => unwrap(await api.post(`/admin/users/${userId}/security/unfreeze`));
