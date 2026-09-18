185c638d084cf2044245318eac03d9abc1445ddd
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-18 15:45:01 +0530

    feat: add admin user status management service

---
+++ Admin/src/services/user.service.js
@@ -0,0 +1,38 @@
+import api from "./api";
+
+const ADMIN_USERS = "/admin/users";
+
+export const listAdminUsers = async (params = {}) => {
+  const response = await api.get(ADMIN_USERS, { params });
+  return response.data.data;
+};
+
+export const getAdminUser = async (userId) => {
+  const response = await api.get(`${ADMIN_USERS}/${userId}`);
+  return response.data.data;
+};
+
+export const blockAdminUser = async (userId, reason) => {
+  const response = await api.post(`${ADMIN_USERS}/${userId}/block`, { reason });
+  return response.data.data;
+};
+
+export const unblockAdminUser = async (userId) => {
+  const response = await api.post(`${ADMIN_USERS}/${userId}/unblock`);
+  return response.data.data;
+};
+
+export const activateAdminUser = async (userId) => {
+  const response = await api.post(`${ADMIN_USERS}/${userId}/activate`);
+  return response.data.data;
+};
+
+export const getAdminSecurityDetails = async (userId) => {
+  const response = await api.get(`${ADMIN_USERS}/${userId}/security`);
+  return response.data.data;
+};
+
+export const unfreezeAdminUser = async (userId) => {
+  const response = await api.post(`${ADMIN_USERS}/${userId}/security/unfreeze`);
+  return response.data.data;
+};
