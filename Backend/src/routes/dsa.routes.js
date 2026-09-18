3a28ccb3350e1b0ce72cb1cce07c095a833f64e6
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-18 10:53:12 +0530

    feat: add company practice route

--- Backend/src/routes/dsa.routes.js
+++ Backend/src/routes/dsa.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import { getDsaCompanies, getDsaProblem, getDsaProgress, getDsaSeoIndexController, getDsaTopics, listDsaProblems } from "../controllers/dsa.controller.js";
+import { getDsaCompanies, getDsaCompany, getDsaProblem, getDsaProgress, getDsaSeoIndexController, getDsaTopics, listDsaProblems } from "../controllers/dsa.controller.js";
 import { getDsaDailyChallenge, getDsaStudyPlan, getDsaStudyPlans } from "../controllers/dsa.challenge.controller.js";
 
 const router = Router();
@@ -14,6 +14,7 @@ router.get("/problems/:slug", getDsaProblem);
 router.get("/progress", getDsaProgress);
 router.get("/topics", getDsaTopics);
 router.get("/companies", getDsaCompanies);
+router.get("/companies/:slug", getDsaCompany);
 router.get("/daily-challenge", getDsaDailyChallenge);
 router.get("/study-plans", getDsaStudyPlans);
 router.get("/study-plans/:slug", getDsaStudyPlan);
