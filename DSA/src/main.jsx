7cbc385d31c84298573739c74ad426e2af020ee1
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-20 10:15:47 +0530

    feat(dsa): add settings provider

--- DSA/src/main.jsx
+++ DSA/src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import SeoManager from './components/SeoManager.jsx'
+import { SettingsProvider } from './context/SettingsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <SeoManager />
-      <App />
-    </BrowserRouter>
+    <SettingsProvider>
+      <BrowserRouter>
+        <SeoManager />
+        <App />
+      </BrowserRouter>
+    </SettingsProvider>
   </StrictMode>,
 )
