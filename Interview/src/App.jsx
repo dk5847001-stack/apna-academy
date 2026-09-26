cba4e469b925e2d318b2b5a10340a8211a1b1731
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-26 15:36:17 +0530

    feat(interview): add auth provider

--- Interview/src/App.jsx
+++ Interview/src/App.jsx
@@ -3,16 +3,19 @@ import AppRoutes from './routes/AppRoutes'
 import { RouterProvider } from './routes/Router'
 import { InterviewFlowProvider } from './context/InterviewFlowContext'
 import AppErrorBoundary from './components/system/AppErrorBoundary'
+import { AuthProvider } from './context/AuthContext'
 
 export default function App() {
   return (
     <AppErrorBoundary>
       <RouterProvider>
-        <InterviewFlowProvider>
+        <AuthProvider>
+          <InterviewFlowProvider>
           <AppShell>
             <AppRoutes />
           </AppShell>
         </InterviewFlowProvider>
+        </AuthProvider>
       </RouterProvider>
     </AppErrorBoundary>
   )
