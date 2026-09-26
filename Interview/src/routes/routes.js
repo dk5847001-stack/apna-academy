5f4797737446dd5a71b7db44f53e1446825557b3
Dilkhush Kumar <dk5847001@gmail.com>
2026-09-26 21:26:32 +0530

    feat(interview-seo): define public interview question route family

--- Interview/src/routes/routes.js
+++ Interview/src/routes/routes.js
@@ -2,6 +2,7 @@ export const ROUTES = {
   HOME: '/',
   LOGIN: '/login',
   SIGNUP: '/signup',
+  INTERVIEW_QUESTIONS: '/interview-questions',
   INTERVIEW_SETUP: '/interview/setup',
   INTERVIEW_PREPARATION: '/interview/preparation',
   INTERVIEW_ROOM: '/interview/room',
